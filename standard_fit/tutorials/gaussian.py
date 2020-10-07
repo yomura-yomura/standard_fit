7d3c8ad49418349616ee19bd3dece14a95840b39
Yugo Omura <yomura@YugonoMacBook-puro.local>
2020-10-07 17:41:21 +0900

    modified:   standard_fit/tutorials/gaussian.py

--- standard_fit/tutorials/gaussian.py
+++ standard_fit/tutorials/gaussian.py
@@ -5,4 +5,4 @@ import standard_fit.plotly.express
 
 if __name__ == "__main__":
     x = np.random.normal(3, 2, size=1_000_000)
-    standard_fit.plotly.express.histogram(x, do_fit=True, fit_stats=True).show()
+    standard_fit.plotly.express.histogram(x, fit_type="gaussian", fit_stats=True).show()
